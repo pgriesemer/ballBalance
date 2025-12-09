da4c0fb137b3d4543b397c4016cd58d214ed6231
Griesemer <PGriesemer@umhb.edu>
2025-12-09 12:14:03 -0600

    first

---
+++ hsvPicker.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+cap = cv2.VideoCapture(0)
+
+# Create a window
+cv2.namedWindow('image')
+
+# create trackbars for color change
+cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
+cv2.createTrackbar('SMin','image',0,255,nothing)
+cv2.createTrackbar('VMin','image',0,255,nothing)
+cv2.createTrackbar('HMax','image',0,179,nothing)
+cv2.createTrackbar('SMax','image',0,255,nothing)
+cv2.createTrackbar('VMax','image',0,255,nothing)
+
+# Set default value for MAX HSV trackbars.
+cv2.setTrackbarPos('HMax', 'image', 179)
+cv2.setTrackbarPos('SMax', 'image', 255)
+cv2.setTrackbarPos('VMax', 'image', 255)
+
+# Initialize to check if HSV min/max value changes
+hMin = sMin = vMin = hMax = sMax = vMax = 0
+phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+
+
+wait_time = 33
+
+
+while(1):
+    # get current positions of all trackbars
+    hMin = cv2.getTrackbarPos('HMin','image')
+    sMin = cv2.getTrackbarPos('SMin','image')
+    vMin = cv2.getTrackbarPos('VMin','image')
+
+    hMax = cv2.getTrackbarPos('HMax','image')
+    sMax = cv2.getTrackbarPos('SMax','image')
+    vMax = cv2.getTrackbarPos('VMax','image')
+
+    # Set minimum and max HSV values to display
+    lower = np.array([hMin, sMin, vMin])
+    upper = np.array([hMax, sMax, vMax])
+
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of white color in HSV
+    # change it according to your need !
+  
+
+    # Threshold the HSV image to get only white colors
+    mask = cv2.inRange(hsv, lower, upper)
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+
+
+    if cv2.waitKey(11) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
